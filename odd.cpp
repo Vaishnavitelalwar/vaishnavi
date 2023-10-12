e81eba6699feca2fd84bd32aa38a64446f1a3d5d
Vaishnavitelalwar <147582767+Vaishnavitelalwar@users.noreply.github.com>
2023-10-12 10:16:25 +0530

    Add files via upload

---
+++ odd.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include<iostream>
+using namespace std;
+int main()
+{
+for (int i=1;i<=20;i++)
+{
+        if(i % 2 == 1)
+cout<<i<<"no is odd\n"<<endl;
+
+
+}
+  return 0;
+}
